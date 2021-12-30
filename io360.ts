d193c7719f7d4edf91ea4eecdd4eca0e102a2d68
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-12-30 16:40:17 +0900

    更新 io360.ts

--- io360.ts
+++ io360.ts
@@ -110,6 +110,229 @@ enum LED_wait {
 let io_neo = neopixel.create(DigitalPin.P9, 3, NeoPixelMode.RGB);
 
 
+//% color="#ff4500" weight=94 block="iːo(ｲｰｵ)専用"
+
+namespace newio_blocks {
+
+
+
+    //% color="#4741f1" weight=89 blockId=neopixel_blue block="iːo青信号 点灯|%mode|" group="1 iːoネオピクセル"
+    export function neopixel_blue_block(mode: onoff) {
+        switch (mode) {
+            case onoff.ON:
+                io_neo.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+                io_neo.show()
+                basic.pause(10);
+                break;
+
+            case onoff.OFF:
+                io_neo.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+                io_neo.show()
+                basic.pause(10);
+                break;
+        }
+    }
+
+    //% color="#ffa800" weight=86 blockId=neopixel_yellow block="iːo黄信号 点灯|%mode|" group="1 iːoネオピクセル"
+    export function neopixel_yellow_block(mode: onoff) {
+        switch (mode) {
+            case onoff.ON:
+                io_neo.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+                io_neo.show()
+                basic.pause(10);
+                break;
+
+            case onoff.OFF:
+                io_neo.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+                io_neo.show()
+                basic.pause(10);
+                break;
+        }
+    }
+
+    //% color="#ff4940" weight=84 blockId=neopixel_red block="iːo赤信号 点灯|%mode|" group="1 iːoネオピクセル"
+    export function neopixel_red_block(mode: onoff) {
+        switch (mode) {
+            case onoff.ON:
+                io_neo.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+                io_neo.show()
+                basic.pause(10);
+                break;
+
+            case onoff.OFF:
+                io_neo.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+                io_neo.show()
+                basic.pause(10);
+                break;
+        }
+    }
+
+    //% color="#20b2aa" weight=82 blockId=neopixel_select block="ﾌﾙｶﾗｰLED |%neo_color| 色で |%neo_number|個つける" group="1 iːoネオピクセル"
+    //% neo_number.min=0 neo_number.max=3
+    export function neopixel_select_block(neo_color: neoLED_color, neo_number: number) {
+        for (let n = 0; n < 2; n++) {
+            io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
+        }
+        io_neo.show()
+        switch (neo_color) {
+            case neoLED_color.赤:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Red))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.だいだい:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Orange))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.黄:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Yellow))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.緑:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Green))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.青:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Blue))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.あい:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Indigo))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.すみれ:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Violet))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.紫:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Purple))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.白:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.White))
+                }
+                io_neo.show()
+                break;
+            case neoLED_color.黒:
+                for (let n = 0; n < neo_number; n++) {
+                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
+                }
+                io_neo.show()
+                break;
+        }
+    }
+    //% color="#9400d3" weight=81 blockId=neopixel_reinbow block="にじ色にする" group="1 iːoネオピクセル"
+    export function neopixel_rainbow() {
+        io_neo.showRainbow(1, 180)
+    }
+
+
+
+
+    //% color="#cd853f" weight=80 blockId=neopixel_erace block="ﾌﾙｶﾗｰLEDを全部消す" group="1 iːoネオピクセル"
+    export function neopixel_erace_block() {
+        for (let n = 0; n < 3; n++) {
+            io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
+        }
+        io_neo.show()
+    }
+
+
+
+    //% color="#1E90FF" weight=83 block="待ち時間（秒）|%second|" group="1 iːoネオピクセル"
+    //% second.min=0 second.max=10
+    export function driveForwards(second: number): void {
+        basic.pause(second * 1000);
+    }
+
+
+    //% color="#a0522d" weight=36 block="iːo人が動いたら" group="2 iːo人感センサー"
+    export function IO_humanDetection(): boolean {
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+        if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //% color="#a0522d" weight=34 blockId=IO_human block="iːo人感ｾﾝｻ値" group="2 iːo人感センサー"
+    export function IO_human(): number {
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+        return pins.digitalReadPin(DigitalPin.P14);
+    }
+
+    //% color="#a0522d"  weight=79 blockId=IO_human_DISP block="iːo人感ｾﾝｻの値を表示する" group="2 iːo人感センサー"
+    export function IO_human_DISP() {
+
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+        basic.showNumber(pins.digitalReadPin(DigitalPin.P14));
+    }
+
+    //% color="#009A00"  weight=81 blockId=microbit2_decideLight block="m:bit光ｾﾝｻ値 |%limit| より暗い" group="3 microbitの光ｾﾝｻ"
+    //% limit.min=0 limit.max=100
+    export function microbit2_decideLight(limit: number): boolean {
+        if (input.lightLevel() / 254 * 100 < limit) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    //% color="#009A00"  weight=80 blockId=microbit2_denkitemp block="m:bit光ｾﾝｻ値" group="3 microbitの光ｾﾝｻ"
+    export function microbit2_denkitemp(): number {
+
+        return Math.round(input.lightLevel() / 254 * 100);
+
+    }
+
+
+    //% color="#228b22"  weight=82 blockId=microbit2_denkiLED block="m:bit光ｾﾝｻの値を表示する" group="3 microbitの光ｾﾝｻ"
+    export function microbit2_denkiLED() {
+        basic.showNumber(Math.round(input.lightLevel() / 254 * 100));
+    }
+
+
+    //% color="#696969" weight=58 blockId=IO_relay block="iːoﾘﾚｰ(ﾃﾞｼﾞﾀﾙ) |%mode|" group="4 iːoリレー"
+    export function IO_relay(mode: onoff) {
+        switch (mode) {
+            case onoff.ON: {
+                return pins.digitalWritePin(DigitalPin.P8, 1);
+            }
+            case onoff.OFF: {
+                return pins.digitalWritePin(DigitalPin.P8, 0);
+            }
+        }
+    }
+    //% color="#696969" weight=56 blockId=IO_relay_2 block="iːoﾘﾚｰ(ｱﾅﾛｸﾞ) |%syuturyoku|" group="4 iːoリレー"
+    //% syuturyoku.min=0 syuturyoku.max=1023
+    export function IO_relay_2(syuturyoku: number) {
+        return pins.analogWritePin(AnalogPin.P8, syuturyoku);
+    }
+}
+
+
+
+
+//% color="#0000ff" weight=93 block="たん体ﾕﾆｯﾄ" 
 namespace eureka_blocks_soro {
 
     //% color="#4169e1" weight=50 blockId=eureka_O2check block="酸素センサー電池チェック ﾎﾟｰﾄ|%pin|" group="酸素センサー"
@@ -1163,7 +1386,7 @@ namespace eureka_blocks_soro {
     }
 
 
-//% color="#0000ff" weight=93 block="たん体ﾕﾆｯﾄ" 
+
 
 
 
@@ -1173,221 +1396,3 @@ namespace eureka_blocks_soro {
 
 }
 
-//% color="#ff4500" weight=94 block="iːo(ｲｰｵ)専用"
-
-namespace newio_blocks {
-
-
-
-    //% color="#4741f1" weight=89 blockId=neopixel_blue block="iːo青信号 点灯|%mode|" group="1 iːoネオピクセル"
-    export function neopixel_blue_block(mode: onoff) {
-        switch (mode) {
-            case onoff.ON:
-                io_neo.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-                io_neo.show()
-                basic.pause(10);
-                break;
-
-            case onoff.OFF:
-                io_neo.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-                io_neo.show()
-                basic.pause(10);
-                break;
-        }
-    }
-
-    //% color="#ffa800" weight=86 blockId=neopixel_yellow block="iːo黄信号 点灯|%mode|" group="1 iːoネオピクセル"
-    export function neopixel_yellow_block(mode: onoff) {
-        switch (mode) {
-            case onoff.ON:
-                io_neo.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-                io_neo.show()
-                basic.pause(10);
-                break;
-
-            case onoff.OFF:
-                io_neo.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-                io_neo.show()
-                basic.pause(10);
-                break;
-        }
-    }
-
-    //% color="#ff4940" weight=84 blockId=neopixel_red block="iːo赤信号 点灯|%mode|" group="1 iːoネオピクセル"
-    export function neopixel_red_block(mode: onoff) {
-        switch (mode) {
-            case onoff.ON:
-                io_neo.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-                io_neo.show()
-                basic.pause(10);
-                break;
-
-            case onoff.OFF:
-                io_neo.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-                io_neo.show()
-                basic.pause(10);
-                break;
-        }
-    }
-
-    //% color="#20b2aa" weight=82 blockId=neopixel_select block="ﾌﾙｶﾗｰLED |%neo_color| 色で |%neo_number|個つける" group="1 iːoネオピクセル"
-    //% neo_number.min=0 neo_number.max=3
-    export function neopixel_select_block(neo_color: neoLED_color, neo_number: number) {
-        for (let n = 0; n < 2; n++) {
-            io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
-        }
-        io_neo.show()
-        switch (neo_color) {
-            case neoLED_color.赤:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Red))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.だいだい:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Orange))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.黄:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Yellow))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.緑:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Green))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.青:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Blue))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.あい:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Indigo))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.すみれ:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Violet))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.紫:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Purple))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.白:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.White))
-                }
-                io_neo.show()
-                break;
-            case neoLED_color.黒:
-                for (let n = 0; n < neo_number; n++) {
-                    io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
-                }
-                io_neo.show()
-                break;
-        }
-    }
-    //% color="#9400d3" weight=81 blockId=neopixel_reinbow block="にじ色にする" group="1 iːoネオピクセル"
-    export function neopixel_rainbow() {
-        io_neo.showRainbow(1, 180)
-    }
-
-
-
-
-    //% color="#cd853f" weight=80 blockId=neopixel_erace block="ﾌﾙｶﾗｰLEDを全部消す" group="1 iːoネオピクセル"
-    export function neopixel_erace_block() {
-        for (let n = 0; n < 3; n++) {
-            io_neo.setPixelColor(n, neopixel.colors(NeoPixelColors.Black))
-        }
-        io_neo.show()
-    }
-
-
-
-    //% color="#1E90FF" weight=83 block="待ち時間（秒）|%second|" group="1 iːoネオピクセル"
-    //% second.min=0 second.max=10
-    export function driveForwards(second: number): void {
-        basic.pause(second * 1000);
-    }
-
-
-    //% color="#a0522d" weight=36 block="iːo人が動いたら" group="2 iːo人感センサー"
-    export function IO_humanDetection(): boolean {
-        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-        if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //% color="#a0522d" weight=34 blockId=IO_human block="iːo人感ｾﾝｻ値" group="2 iːo人感センサー"
-    export function IO_human(): number {
-        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-        return pins.digitalReadPin(DigitalPin.P14);
-    }
-
-    //% color="#a0522d"  weight=79 blockId=IO_human_DISP block="iːo人感ｾﾝｻの値を表示する" group="2 iːo人感センサー"
-    export function IO_human_DISP() {
-
-        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-        basic.showNumber(pins.digitalReadPin(DigitalPin.P14));
-    }
-
-    //% color="#009A00"  weight=81 blockId=microbit2_decideLight block="m:bit光ｾﾝｻ値 |%limit| より暗い" group="3 microbitの光ｾﾝｻ"
-    //% limit.min=0 limit.max=100
-    export function microbit2_decideLight(limit: number): boolean {
-        if (input.lightLevel() / 254 * 100 < limit) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-
-    //% color="#009A00"  weight=80 blockId=microbit2_denkitemp block="m:bit光ｾﾝｻ値" group="3 microbitの光ｾﾝｻ"
-    export function microbit2_denkitemp(): number {
-
-        return Math.round(input.lightLevel() / 254 * 100);
-
-    }
-
-
-    //% color="#228b22"  weight=82 blockId=microbit2_denkiLED block="m:bit光ｾﾝｻの値を表示する" group="3 microbitの光ｾﾝｻ"
-    export function microbit2_denkiLED() {
-        basic.showNumber(Math.round(input.lightLevel() / 254 * 100));
-    }
-
-
-    //% color="#696969" weight=58 blockId=IO_relay block="iːoﾘﾚｰ(ﾃﾞｼﾞﾀﾙ) |%mode|" group="4 iːoリレー"
-    export function IO_relay(mode: onoff) {
-        switch (mode) {
-            case onoff.ON: {
-                return pins.digitalWritePin(DigitalPin.P8, 1);
-            }
-            case onoff.OFF: {
-                return pins.digitalWritePin(DigitalPin.P8, 0);
-            }
-        }
-    }
-    //% color="#696969" weight=56 blockId=IO_relay_2 block="iːoﾘﾚｰ(ｱﾅﾛｸﾞ) |%syuturyoku|" group="4 iːoリレー"
-    //% syuturyoku.min=0 syuturyoku.max=1023
-    export function IO_relay_2(syuturyoku: number) {
-        return pins.analogWritePin(AnalogPin.P8, syuturyoku);
-    }
-}
